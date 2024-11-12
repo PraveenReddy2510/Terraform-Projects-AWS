1ae33660a1623b1fb5e7548b691d85bc25b93e16
Praveen Reddy <rpraveen4712@gmail.com>
2024-11-12 21:49:24 +0530

    com

--- 3-tier_Architecture-1/modules/security/main.tf
+++ 3-tier_Architecture-1/modules/security/main.tf
@@ -1,10 +1,10 @@
-resource "aws_security_group" "eks" {
-  vpc_id = var.vpc_id
-  ingress {
-    ...
-  }
-  egress {
-    ...
-  }
-}
+#resource "aws_security_group" "eks" {
+#  vpc_id = var.vpc_id
+#  ingress {
+#    ...
+#  }
+#  egress {
+#    ...
+#  }
+#}
 
--- 3-tier_Architecture-1/modules/storage/main.tf
+++ 3-tier_Architecture-1/modules/storage/main.tf
@@ -1,10 +1,10 @@
-resource "aws_s3_bucket" "app_bucket" {
-  bucket = "app-bucket"
-}
+# #resource "aws_s3_bucket" "app_bucket" {
+# #  bucket = "app-bucket"
+# }
 
-resource "aws_db_instance" "postgresql" {
-  engine         = "postgres"
-  instance_class = "db.t3.micro"
-  ...
-}
+# resource "aws_db_instance" "postgresql" {
+#   engine         = "postgres"
+#   instance_class = "db.t3.micro"
+#   ...
+# }
 
