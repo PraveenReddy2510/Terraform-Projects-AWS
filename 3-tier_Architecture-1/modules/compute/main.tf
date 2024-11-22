bb67a26ce63ffe3743a4580dbd5e867a130fa61d
Praveen Reddy <rpraveen4712@gmail.com>
2024-11-22 14:24:00 +0530

    vpc-changed

--- 3-tier_Architecture-1/modules/compute/main.tf
+++ 3-tier_Architecture-1/modules/compute/main.tf
@@ -14,7 +14,7 @@ module "eks" {
     vpc-cni                = {}
   }
 
-  vpc_id                   = "vpc-1234556abcdef"
+  vpc_id                   = aws_vpc.main.vpc_id
   subnet_ids               = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghi345a"]
   control_plane_subnet_ids = ["subnet-xyzde987", "subnet-slkjf456", "subnet-qeiru789"]
 
